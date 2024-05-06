c7a66492e8d3b50499e76ab75c825c66c32b9ded
huiyu0312 <u11002167@o365.ttu.edu.tw>
2024-05-06 17:04:45 +0800

    我全部寫完了，包含心得
    這次的程設作業對我來說是個極大的挑戰，除了要學會用git hub繳交之外，還有學會顯示直角三角形以及乘法表，對於函數也熟悉了不少。

--- HW2.cpp
+++ HW2.cpp
@@ -3,6 +3,7 @@
 #include<conio.h>//前置處理器的一種,將conio檔案包含進去
 char menu;//宣告主選單的變數 
 void mult (int n);//宣告乘法表的函數
+void tri (char ch);//宣告乘法表的函數
 int main(void)//主函數
 {
 	printf("---------------------------------------------\n");
@@ -68,67 +69,68 @@ int main(void)//主函數
 		    scanf("%c",&menu);//讀入menu的值 
 		    switch(menu)//選擇要做哪一個 
 		    {
-		      case'a':
-		      case'A':
-		      {
-			     system("cls");//清除螢幕 
-			     printf("輸入一個'a'到'n'的字元\n");//要求使用者輸入一個‘a’到‘n'的字元 
-			     scanf("%c",&ch);//讀入ch的值 
-			     if(ch>='a'||ch<='n')//若ch不在'a'到'n'之間 
-			      {
-				     
-			      }
-			     else
-			      {
-				     printf("輸入錯誤，請重新輸入");//顯示輸入錯誤，請重新輸入
-			      }
-				 system("pause");//螢幕畫面暫停，並等待使用者按任意鍵 
-				 break;//離開迴圈 
-			  } 
-			  case'b':
-			  case'B':
-			  {
-				 fflush(stdin);//使input buffer淨空  
-				 system("cls");//清除螢幕
-				 int n;//宣告整數變數 
-				 printf("請輸入一個1至9的整數\n");//要求使用者輸入一個1至9的整數
-                 scanf("%d",&n);//讀入n的值 
-				 if(n>=1&&n<=9)//如果n為1~9的整數 
-				 {
-				 	mult(n);//執行乘法表 
-				 }
-				 else
-				 {
-				 	printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
-				 }
-				 system("pause");//螢幕畫面暫停，並等待使用者按任意鍵
-				 break;//離開迴圈
-			  }
-			  case'c':
-			  case'C':
-			  {
-				aa:
-				printf("continue?(y/n)");//顯示是否要繼續 
-			  	char choice=getch();//宣告choice=上面那行的字元 
-			  	switch(choice)
-				  {
-				  	case'y':
-				  	case'Y':
-					 {
-						break;//離開迴圈
-					 }
-					case'n':
-				  	case'N':
-					  {
-						return 0;
-						break;//離開迴圈
-					   }
-					default: 
+		    	case'a':
+		    	case'A':
+		    	{
+					system("cls");//清除螢幕 
+			    	printf("輸入一個'a'到'n'的字元\n");//要求使用者輸入一個‘a’到‘n'的字元
+					fflush(stdin); 
+			    	scanf("%c",&ch);//讀入ch的值 
+			    	if(ch>='a'||ch<='n')//若ch在'a'到'n'之間 
+			    	{
+				    	tri(ch); 
+			    	}
+			    	else
+			    	{
+				    	printf("輸入錯誤，請重新輸入");//顯示輸入錯誤，請重新輸入
+			    	}
+					system("pause");//螢幕畫面暫停，並等待使用者按任意鍵 
+					break;//離開迴圈 
+			  	} 
+			  	case'b':
+			  	case'B':
+			  	{
+					fflush(stdin);//使input buffer淨空  
+					system("cls");//清除螢幕
+					int n;//宣告整數變數 
+					printf("請輸入一個1至9的整數\n");//要求使用者輸入一個1至9的整數
+                	scanf("%d",&n);//讀入n的值 
+					if(n>=1&&n<=9)//如果n為1~9的整數 
 					{
-						printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
-						goto aa;//回到詢問是否要繼續那行程式 
-					}
-			   }
+				 		mult(n);//執行乘法表 
+				 	}
+				 	else
+				 	{
+				 		printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
+				 	}
+				 	system("pause");//螢幕畫面暫停，並等待使用者按任意鍵
+				 	break;//離開迴圈
+			  	}
+			  	case'c':
+			  	case'C':
+			  	{
+					aa:
+					printf("continue?(y/n)");//顯示是否要繼續 
+			  		char choice=getch();//宣告choice=上面那行的字元 
+			  		switch(choice)
+				  	{
+				  		case'y':
+				  		case'Y':
+					 	{
+							break;//離開迴圈
+					 	}
+						case'n':
+				  		case'N':
+					  	{
+							return 0;
+							break;//離開迴圈
+					   	}
+						default: 
+						{
+							printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
+							goto aa;//回到詢問是否要繼續那行程式 
+						}
+			   	}
 				break;//離開迴圈
 			  }
 	    	}
@@ -150,4 +152,19 @@ void mult(int n)
 		printf("\n");
 	}
 }
-
+void tri(char ch)
+{
+	int i,j;
+	for(i=0;i<=ch-'a';i++)
+	{
+		for(j=0;j<=ch-'a';j++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<=i;j++)
+		{
+			printf("%c",ch-j);
+		}
+		printf("\n");
+	}
+}
