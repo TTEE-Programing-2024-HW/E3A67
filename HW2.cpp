e439e26cb0683677582b4382ff1bf96737621cea
huiyu0312 <u11002167@o365.ttu.edu.tw>
2024-05-06 11:56:33 +0800

    我完成1,2,4,5題
    因為我上個檔案不知道為什麼不能編譯，所以又再重建立了一個HW2

--- HW2.cpp
+++ HW2.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>//前置處理器的一種,將stdio檔案包含進去 
 #include<stdlib.h>//前置處理器的一種,將stdlib檔案包含進去
-#include<conio.h>
+#include<conio.h>//前置處理器的一種,將conio檔案包含進去
 char menu;//宣告主選單的變數 
-void mult (int n);//宣告乘法表的函數 
+void mult (int n);//宣告乘法表的函數
 int main(void)//主函數
 {
 	printf("---------------------------------------------\n");
@@ -32,7 +32,7 @@ int main(void)//主函數
     printf("---------------------------------------------\n");//顯示個人畫面 
     system ("pause");//暫停程式執行,等待使用者按下任意鍵繼續 
     system("cls");//清除螢幕
-    int x,y;//宣告整數變數 x,i
+    int x,y;//宣告整數變數 x,y
     char ch;//宣告字元變數 
     while(y<3)//最多輸入3次 
 	{
@@ -41,7 +41,7 @@ int main(void)//主函數
 		if(x!=2024)//若x不等於2024 
 		{
 			printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
-			y++;
+			y++;//y+1
 			if(y==3)//若輸入達三次 
 			{
 			  printf("輸入錯誤達三次，將結束程式\n");//顯示輸入錯誤達三次，將結束程式
@@ -57,7 +57,7 @@ int main(void)//主函數
 	{
 		while(1)
 		{
-			fflush(stdin);
+			fflush(stdin);//使input buffer淨空  
 			system("cls");//清除螢幕
 			printf("--------------------------\n");
 		    printf("|a. 畫出直角三角形       |\n");
@@ -65,34 +65,34 @@ int main(void)//主函數
 		    printf("|c. 結束                 |\n");
 		    printf("--------------------------\n");//顯示主選單
 		    printf("請根據選單上的字元以執行程式:");//要求使用者輸入一個選單上的字元 
-		    scanf("%c",&menu);
+		    scanf("%c",&menu);//讀入menu的值 
 		    switch(menu)//選擇要做哪一個 
 		    {
 		      case'a':
 		      case'A':
 		      {
-			     system("cls"); 
+			     system("cls");//清除螢幕 
 			     printf("輸入一個'a'到'n'的字元\n");//要求使用者輸入一個‘a’到‘n'的字元 
-			     scanf("%c",&ch);
-			     if(ch>='a'||ch<='n')
+			     scanf("%c",&ch);//讀入ch的值 
+			     if(ch>='a'||ch<='n')//若ch不在'a'到'n'之間 
 			      {
-				    
+				     
 			      }
 			     else
 			      {
 				     printf("輸入錯誤，請重新輸入");//顯示輸入錯誤，請重新輸入
 			      }
-				 system("pause");
-				 break;
+				 system("pause");//螢幕畫面暫停，並等待使用者按任意鍵 
+				 break;//離開迴圈 
 			  } 
 			  case'b':
 			  case'B':
 			  {
-				 fflush(stdin);
-				 system("cls");
-				 int n;
+				 fflush(stdin);//使input buffer淨空  
+				 system("cls");//清除螢幕
+				 int n;//宣告整數變數 
 				 printf("請輸入一個1至9的整數\n");//要求使用者輸入一個1至9的整數
-                 scanf("%d",&n);
+                 scanf("%d",&n);//讀入n的值 
 				 if(n>=1&&n<=9)//如果n為1~9的整數 
 				 {
 				 	mult(n);//執行乘法表 
@@ -101,8 +101,8 @@ int main(void)//主函數
 				 {
 				 	printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
 				 }
-				 system("pause");
-				 break;
+				 system("pause");//螢幕畫面暫停，並等待使用者按任意鍵
+				 break;//離開迴圈
 			  }
 			  case'c':
 			  case'C':
@@ -115,13 +115,13 @@ int main(void)//主函數
 				  	case'y':
 				  	case'Y':
 					 {
-						break;
+						break;//離開迴圈
 					 }
 					case'n':
 				  	case'N':
 					  {
 						return 0;
-						break;
+						break;//離開迴圈
 					   }
 					default: 
 					{
@@ -129,7 +129,7 @@ int main(void)//主函數
 						goto aa;//回到詢問是否要繼續那行程式 
 					}
 			   }
-				break;
+				break;//離開迴圈
 			  }
 	    	}
 		    
@@ -150,4 +150,4 @@ void mult(int n)
 		printf("\n");
 	}
 }
- 
+
