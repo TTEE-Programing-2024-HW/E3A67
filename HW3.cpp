c4d27bd2a184b7be1d13e17033e71989fece19f0
huiyu0312 <u11002167@o365.ttu.edu.tw>
2024-05-21 02:25:17 +0800

    我完成作業了
    心得:這次的作業比起上次又困難了不少，花了很多時間去想仍然還是有一題寫不出來，甚至過程中也編譯不出來，重弄了一次才成功，但也讓我對c語言有更深一層的認識。

--- HW3.cpp
+++ HW3.cpp
@@ -1,56 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include<conio.h>
+#include<stdio.h>//前置處理器的一種,將stdio檔案包含進去
+#include<stdlib.h>//前置處理器的一種,將stdlib檔案包含進去
+#include<string.h>//前置處理器的一種,將string檔案包含進去
+#include<time.h>//前置處理器的一種,將time檔案包含進去
 
 #define ROW 9
 #define COLUMN 9
 
 char seatlist[ROW][COLUMN];//定義二維陣列 
 
-void seat(void);
-void seatprint(void);
+void style(void);//個人畫面
+ 
+void seat(void);//座位表 
+void seatprint(void);//座位表
 void reserve_a(int count);
 void reserve_b(int count);
 
+
 int main(void)
 {
-    char menu;
-    
-    printf("---------------------------------------------\n");
-	printf("---------------------------------------------\n");
-	printf("---------------------------------------------\n");
-	printf("---------------------------------------------\n");
-    printf("---------------------------------------------\n");
-	printf("EEEEEEEEEE      333333333            A       \n");
-	printf("E                       3          A   A     \n");
-	printf("E                       3         A     A    \n");
-	printf("EEEEEEEEEE      333333333        AAAAAAAAA   \n");
-	printf("E                       3       A         A  \n");
-	printf("E                       3      A           A \n");
-	printf("EEEEEEEEEE      333333333     A             A\n");
-	printf("                                             \n");
-	printf("       6666666666      777777777             \n");
-	printf("       6               7       7             \n");
-	printf("       6                       7             \n");
-	printf("       6666666666              7             \n");
-	printf("       6        6              7             \n");
-	printf("       6        6              7             \n");
-	printf("       6666666666              7             \n");
-    printf("---------------------------------------------\n");
-    printf("---------------------------------------------\n");
-    printf("---------------------------------------------\n");
-    printf("---------------------------------------------\n");
-    printf("---------------------------------------------\n");//顯示個人畫面
-    system ("pause");//暫停程式執行,等待使用者按下任意鍵繼續 
-    system("cls");//清除螢幕
-    
-	int x,y=0;
+	char menu;//宣告選單變數 
+	char o; 
+
+	int x,y=0;//宣告密碼和輸入次數的變數 
 	
+	style();
     while(y<3)//最多輸入3次 
 	{
-		printf("enter the password:");//提示使用者輸入四個字的密碼 
+		printf("Enter the password:");//提示使用者輸入四個字的密碼 
         scanf("%d",&x); //讀入使用者輸入的整數
 		if(x!=2024)//若x不等於2024 
 		{
@@ -69,81 +45,94 @@ int main(void)
 	} 
 	if(x==2024)//若x等於2024 
 	{
-	  printf("welcome\n");//顯示welcome
-	  system("pause");
-	  while(1)
-	  {
-	    fflush(stdin);//使input buffer淨空  
-		system("cls");//清除螢幕
-		printf("------[booking system------\n");
-		printf("|a. Available seats       |\n");
-		printf("|b. Arrange for you       |\n");   
-		printf("|c. Choose by yourself    |\n");
-		printf("|d. Exit                  |\n");
-		printf("---------------------------\n");//顯示主選單
-		printf("INPUT a~d\n");
-        scanf(" %c",&menu);
-        
-        seat();
-        seatprint();
-        
-         switch(menu)
-		 {
-           case 'a': 
-             system("cls");
-             reserve_a(10); //預定10個座位 
-             printf("\nSeats reserved:\n");
-             seatprint();
-             break;
-           case 'b':
-             system("cls");
-             printf("How many seats do you need? (1-4): ");
-             int count;
-             scanf("%d", &count);
-             if (count >= 1 && count <= 4) 
-			 {
-			     reserve_b(count); // 預訂座位數量
-                 printf("\nSeats reserved:\n");
-                 seatprint();
-             } 
-			 else
-			 {
-                 printf("Invalid number of seats.\n");
-             }
-             break;
-           case 'c':
-           	break;
-		   case 'd':
-		   {
-				aa:
-					printf("continue?(y/n)");//顯示是否要繼續 
-			  		char choice=getch();//宣告choice=上面那行的字元 
-			  		switch(choice)
-					  {
-					    case'y':
-					 	{
-							break;//離開迴圈
-					 	}
-					    case'n':
-					  	{
-							return 0;
-							break;//離開迴圈
-					   	}
-					    default: 
+		printf("welcome\n");//顯示welcome
+		system("pause");
+	  	while(1)
+	 	{
+		   	fflush(stdin);//使input buffer淨空  
+			system("cls");//清除螢幕
+			printf("------[booking system------\n");
+			printf("|   a. Available seats    |\n");
+			printf("|   b. Arrange for you    |\n");   
+			printf("|   c. Choose by yourself |\n");
+			printf("|   d. Exit               |\n");
+			printf("---------------------------\n");//顯示主選單
+			
+			seat();
+	        seatprint();
+			
+			printf("INPUT a~d\n");//提醒使用者輸入a~d 
+	        scanf(" %c",&menu);//讀入上方輸入的值 
+	        
+	        switch(menu)
+		    {
+		        case 'a': 
+		            system("cls");
+		            reserve_a(10); //預定10個座位 
+		            printf("\nSeats reserved:\n");
+		            seatprint();
+		            
+		            system("pause");
+		            break;
+		            
+		        case 'b':
+		            system("cls");
+		            printf("How many seats do you need? (1-4): ");
+		            int count;
+		            scanf("%d", &count);
+		            if (count >= 1 && count <= 4) 
+					{
+		                reserve_b(count); // 預訂座位數量
+		                printf("\nSeats reserved:\n");
+		                seatprint();
+		            } 
+					else 
+					{
+		                printf("Invalid number of seats.\n");
+		            }
+		            
+		            system("pause");
+		            break;
+		            
+		        case 'c':
+		        	
+		        	break;
+		        case 'd':
+		        	fflush(stdin);
+			        printf("Continue? (y/n)\n");
+					while(scanf("%c",&o))
+					{
+						if(o=='y')//判斷輸入是y還是n 
 						{
-							printf("input error,please input again\n");//顯示輸入錯誤，請重新輸入
-							goto aa;//回到詢問是否要繼續那行程式 
+							break;
 						}
-			   	      }
-			break;
-		   }	
-         }
-
-	  }
-	  
-	  system("pause");
-      return 0;
-    }
+						else if(o=='n')
+						{
+							printf("thank for using\n");
+							
+							system("pause");
+							
+							return 0;
+						}
+						else
+						{ 
+							printf("error range\n");
+							printf("input again\n\a");
+						} 
+					}
+					system("pause");
+					break;
+					
+				default:
+					printf("pleaase input again(a~d)\n\a");
+					
+					system("pause");
+					
+					break;	
+		    }
+		}
+	}
+}
 
 void seat(void)
 {
@@ -156,7 +145,8 @@ void seat(void)
     }
 }
 
-void seatprint(void){
+void seatprint(void)
+{
     printf("\\");
     for (int j = 1; j <= COLUMN; ++j) 
     {
@@ -175,7 +165,8 @@ void seatprint(void){
     }
 }
 
-void reserve_a(int count) {
+void reserve_a(int count) 
+{
     srand(time(NULL));
     int reservedCount = 0;
     while (reservedCount < count) 
@@ -226,7 +217,8 @@ void reserve_a(int count) {
     }
 }
 
-void reserve_b(int count) {
+void reserve_b(int count) 
+{
     srand(time(NULL));
     int reservedCount = 0;
     while (reservedCount < count) 
@@ -276,5 +268,35 @@ void reserve_b(int count) {
         }
     }
 }
-
+void style(void)
+{
+	printf("---------------------------------------------\n");
+	printf("---------------------------------------------\n");
+	printf("---------------------------------------------\n");
+	printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+	printf("EEEEEEEEEE      333333333            A       \n");
+	printf("E                       3          A   A     \n");
+	printf("E                       3         A     A    \n");
+	printf("EEEEEEEEEE      333333333        AAAAAAAAA   \n");
+	printf("E                       3       A         A  \n");
+	printf("E                       3      A           A \n");
+	printf("EEEEEEEEEE      333333333     A             A\n");
+	printf("                                             \n");
+	printf("       6666666666      777777777             \n");
+	printf("       6               7       7             \n");
+	printf("       6                       7             \n");
+	printf("       6666666666              7             \n");
+	printf("       6        6              7             \n");
+	printf("       6        6              7             \n");
+	printf("       6666666666              7             \n");
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");//顯示個人畫面
+    system ("pause");//暫停程式執行,等待使用者按下任意鍵繼續 
+    system("cls");//清除螢幕
+    
+}
 
