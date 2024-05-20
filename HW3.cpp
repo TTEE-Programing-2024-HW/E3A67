a3aab3860805a1ea3850aad3ca733a56186b85d9
huiyu0312 <u11002167@o365.ttu.edu.tw>
2024-05-20 20:52:25 +0800

    我完成了12346題
    我多寫了第34題，但一直沒辦法編譯，一直說我函數定義有問題

--- HW3.cpp
+++ HW3.cpp
@@ -1,16 +1,24 @@
-#include<stdio.h>//前置處理器的一種,將stdio檔案包含進去
-#include<stdlib.h>//前置處理器的一種,將stdlib檔案包含進去
-#include<conio.h>//前置處理器的一種,將conio檔案包含進去
-#define row 9
-#define col 9
-char menu,seat[row][col],ch;//宣告變數
-int x,y=0;//宣告整數變數 
-void show1(char seat[row][col]);
-void show2(char seat[row][col]);
-void show3(char seat[row][col]);
-int main(void)//主函數 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<conio.h>
+
+#define ROW 9
+#define COLUMN 9
+
+char seatlist[ROW][COLUMN];//定義二維陣列 
+
+void seat(void);
+void seatprint(void);
+void reserve_a(int count);
+void reserve_b(int count);
+
+int main(void)
 {
-	printf("---------------------------------------------\n");
+    char menu;
+    
+    printf("---------------------------------------------\n");
 	printf("---------------------------------------------\n");
 	printf("---------------------------------------------\n");
 	printf("---------------------------------------------\n");
@@ -37,6 +45,9 @@ int main(void)//主函數
     printf("---------------------------------------------\n");//顯示個人畫面
     system ("pause");//暫停程式執行,等待使用者按下任意鍵繼續 
     system("cls");//清除螢幕
+    
+	int x,y=0;
+	
     while(y<3)//最多輸入3次 
 	{
 		printf("enter the password:");//提示使用者輸入四個字的密碼 
@@ -59,7 +70,7 @@ int main(void)//主函數
 	if(x==2024)//若x等於2024 
 	{
 	  printf("welcome\n");//顯示welcome
-	  system("pause");	
+	  system("pause");
 	  while(1)
 	  {
 	    fflush(stdin);//使input buffer淨空  
@@ -70,211 +81,200 @@ int main(void)//主函數
 		printf("|c. Choose by yourself    |\n");
 		printf("|d. Exit                  |\n");
 		printf("---------------------------\n");//顯示主選單
-		printf("請根據選單上的字元以執行程式:");//要求使用者輸入一個選單上的字元 
-		scanf("%c",&menu);//讀入menu的值
-		 switch(menu)//選擇要做哪一個 
-		    {
-		    	case'a':
-		    	{
-				  show1(seat);
-				  show2(seat);
-				  show3(seat);	
-			  	} 
-			  	case'b':
-			  	{
-					fflush(stdin);//使input buffer淨空  
-					system("cls");//清除螢幕
-					int p;
-				    printf("請輸入人數(1~4):");
-			        scanf("%d",&p);
-			        while(p<1||p>4)  /*判斷是否是要求中的人數*/
-					{
-						system("CLS");
-				        printf("人數錯誤，請重新輸入！\n");  /*判斷為否，要求使用者重新輸入*/
-				        printf("請輸入人數(1~4):");
-				        scanf("%d",&p);
-			        }
-			        system("CLS");
-			        int c=0,k,max=9,min=0;
-			        while (c<=1)  /*設定電腦排位次數的上限*/
-					{
-				      while(k!=1)  /*產生亂數座位*/
-					 	{ 
-					 	  int m,n;
-					      m=rand()%(max-min+1)+min;
-					      n=rand()%(max-min+1)+min;
-					      if(seat[m][n]!=1)  /*將產生的亂數座位轉成被選中的座位*/
-						  { 
-							    if(p==1)
-								{
-							         seat[m][n]=2;
-						             k=1;
-						        }
-						        else if(p==2&&(n+1)<=8&&seat[m][n+1]!=1)
-								{
-							         seat[m][n]=2;
-							         seat[m][n+1]=2;
-						             k=1;
-						        }
-						        else if(p==3&&(n+2)<=8&&seat[m][n+1]!=1&&seat[m][n+2]!=1)
-						        {
-							         seat[m][n]=2;
-							         seat[m][n+1]=2;
-							         seat[m][n+2]=2;
-							         k=1;
-					            }
-						        else if(p==4&&(n+1)<=8&&(m+1)>=0&&seat[m][n+1]!=1&&seat[m+1][n]!=1&&seat[m+1][n+1]!=1)
-					    	    {
-							         seat[m][n]=2;
-							         seat[m+1][n]=2;
-							         seat[m][n+1]=2;
-							         seat[m+1][n+1]=2;
-							         k=1;
-						        }
-				          }  
-					      else
-					      {
-						     k=0;  /*電腦沒有選中符合需求的座位*/ 
-                          }
-				        }
-				        printf("\\123456789\n");  /*印出座位表*/ 
-				        for(int m=8;m>=0;m--)
-				        {
-					        printf("%d",m+1);
-					        for(int n=0;n<=8;n++)
-					        {
-							    if(seat[m][n]==0)
-						        { 
-							      printf("-");
-						        }   
-						        else if(seat[m][n]==1)
-						        { 
-							      printf("*");
-						        } 
-						        else
-						        { 
-							      printf("@");
-					            } 
-					        }
-					        printf("\n");
-				        }
-				        printf("接受此電腦排位嗎(y/n)？\n");  /*要求使用者確認是否繼續*/ 
-				        printf("(不接受次數達兩次將退回主選單)\n");
-				        fflush(stdin);
-				        scanf("%c",&ch);
-			            while(ch!='y'&&ch!='n')  /*判斷是否為要求中的字元*/ 
-				        {
-					      system("CLS");
-					      printf("錯誤訊息，請重新輸入！\n");  /*判斷為否，要求使用者重新輸入*/ 
-					      printf("接受此電腦排位嗎(y/n)？");
-					      scanf("%c",&ch);
-				        }
-				        if(ch=='y')  /*判斷使用者的選擇*/ 
-				        {
-					      for(int m=8;m>=0;m--)  /*將選中的座位轉為已預訂的座位*/ 
-					        {
-						        for(int n=0;n<=8;n++)
-						        {
-							        if(seat[m][n]==2)
-							        {	
-							            seat[m][n]=1;
-							        }	
-						        }
-					        }
-					      k=0;
-					      break;
-				        }
-				        else
-				        {     
-					      c++;
-					      k=0;
-					      for(int m=8;m>=0;m--)  /*將未選中的座位放棄*/ 
-					       {
-						     for(int n=0;n<=8;n++)
-						        {
-							      if(seat[m][n]==2)
-							      { 
-								     seat[m][n]=0;
-						          }    
-						        }
-					        }
-					        system("CLS");
-				        }	
-			        }
-			        printf("\n已選擇兩次不接受，退回主選單。");
-			        printf("\n(按下任意鍵回到主選單)\n");  /*功能結束，提示回到主選單*/ 
-			        break;
-			  	}
-			  	case'c':
-			  	{
-				}
-			  	case'd':
-			  	{
-					aa:
+		printf("INPUT a~d\n");
+        scanf(" %c",&menu);
+        
+        seat();
+        seatprint();
+        
+         switch(menu)
+		 {
+           case 'a': 
+             system("cls");
+             reserve_a(10); //預定10個座位 
+             printf("\nSeats reserved:\n");
+             seatprint();
+             break;
+           case 'b':
+             system("cls");
+             printf("How many seats do you need? (1-4): ");
+             int count;
+             scanf("%d", &count);
+             if (count >= 1 && count <= 4) 
+			 {
+			     reserve_b(count); // 預訂座位數量
+                 printf("\nSeats reserved:\n");
+                 seatprint();
+             } 
+			 else
+			 {
+                 printf("Invalid number of seats.\n");
+             }
+             break;
+           case 'c':
+           	break;
+		   case 'd':
+		   {
+				aa:
 					printf("continue?(y/n)");//顯示是否要繼續 
 			  		char choice=getch();//宣告choice=上面那行的字元 
 			  		switch(choice)
-				  	{
-				  		case'y':
+					  {
+					    case'y':
 					 	{
 							break;//離開迴圈
 					 	}
-						case'n':
+					    case'n':
 					  	{
 							return 0;
 							break;//離開迴圈
 					   	}
-						default: 
+					    default: 
 						{
-							printf("輸入錯誤，請重新輸入\n");//顯示輸入錯誤，請重新輸入
+							printf("input error,please input again\n");//顯示輸入錯誤，請重新輸入
 							goto aa;//回到詢問是否要繼續那行程式 
 						}
-			   	}
-				break;//離開迴圈
-			  }
-	    	}
-		     
+			   	      }
+			break;
+		   }	
+         }
+
 	  }
-	} 
-}
-void show1(char seat[row][col])
+	  
+	  system("pause");
+      return 0;
+    }
+
+void seat(void)
 {
-	for(int m=0;m<row;m++)
-	{
-	  for(int n=0;n<col;n++)
-	  {
-	  	seat[m][n]='-';
-	  }	
-	}
-}
-void show_2(char seat[row][col])
-{
-  int reserved=0;
-  while(reserved < show_2)
-  {
-  	int i=rand()%row;
-  	int j=rand()%col;
-  	if(seat[i][j]=='-')
-	  {
-	  	seat[i][j]=='*';
-	  	reserved++;
-	  }
-  } 	
-}
-void show3(char seat[row][col])
-{
-	for(int m=row-1;m>=0;m--)
-	{
-	    printf("\\123456789\\n");	
-		for(m=row-1;m>=0;m--)
+    for (int i = 0; i < ROW; ++i) 
+    {
+        for (int j = 0; j < COLUMN; ++j) 
         {
-  	      printf("%d",m+1);
-  	      for(int n=0;n<col;n++)
-	      {
-	  	    printf("%c",seat[m][n]);
-	      }
-		}
-	    printf("\n");
-	}
+            seatlist[i][j] = '-';
+        }
+    }
 }
+
+void seatprint(void){
+    printf("\\");
+    for (int j = 1; j <= COLUMN; ++j) 
+    {
+        printf("%d", j);
+    }
+    printf("\n");
+
+    for (int i = ROW; i >= 1; --i) 
+    {
+        printf("%d", i);
+        for (int j = 0; j < COLUMN; ++j) 
+        {
+            printf("%c", seatlist[i - 1][j]);
+        }
+        printf("\n");
+    }
+}
+
+void reserve_a(int count) {
+    srand(time(NULL));
+    int reservedCount = 0;
+    while (reservedCount < count) 
+    {
+        if (count == 4) 
+		{
+            int row, col;
+            if (rand() % 2) 
+			{ //隨機預定1排或正方形 
+                do 
+				{
+                    row = rand() % ROW;
+                    col = rand() % (COLUMN - 3); // 確保4連續 
+                } while (seatlist[row][col] != '-' || seatlist[row][col + 1] != '-' || seatlist[row][col + 2] != '-' || seatlist[row][col + 3] != '-');
+                
+                for (int i = col; i < col + 4; ++i) 
+				{
+                    seatlist[row][i] = '*';
+                }
+            } 
+			else 
+			{
+                do 
+				{
+                    row = rand() % (ROW - 1); // 確保2行連續
+                    col = rand() % (COLUMN - 1); // 確保2列連續
+                } while (seatlist[row][col] != '-' || seatlist[row][col + 1] != '-' || seatlist[row + 1][col] != '-' || seatlist[row + 1][col + 1] != '-');
+                
+                seatlist[row][col] = '*';
+                seatlist[row][col + 1] = '*';
+                seatlist[row + 1][col] = '*';
+                seatlist[row + 1][col + 1] = '*';
+            }
+            reservedCount += 4;
+        } 
+		else 
+		{
+            int row, col;
+            do 
+			{
+                row = rand() % ROW;
+                col = rand() % COLUMN;
+            } while (seatlist[row][col] != '-');
+            
+            seatlist[row][col] = '*';
+            reservedCount++;
+        }
+    }
+}
+
+void reserve_b(int count) {
+    srand(time(NULL));
+    int reservedCount = 0;
+    while (reservedCount < count) 
+    {
+        if (count == 4) 
+		{
+            int row, col;
+            if (rand() % 2) 
+			{ 
+                do 
+				{
+                    row = rand() % ROW;
+                    col = rand() % (COLUMN - 3); 
+                } while (seatlist[row][col] != '-' || seatlist[row][col + 1] != '-' || seatlist[row][col + 2] != '-' || seatlist[row][col + 3] != '-');
+                
+                for (int i = col; i < col + 4; ++i) 
+				{
+                    seatlist[row][i] = '@';
+                }
+            } 
+			else 
+			{
+                do 
+				{
+                    row = rand() % (ROW - 1); 
+                    col = rand() % (COLUMN - 1); 
+                } while (seatlist[row][col] != '-' || seatlist[row][col + 1] != '-' || seatlist[row + 1][col] != '-' || seatlist[row + 1][col + 1] != '-');
+                
+                seatlist[row][col] = '@';
+                seatlist[row][col + 1] = '@';
+                seatlist[row + 1][col] = '@';
+                seatlist[row + 1][col + 1] = '@';
+            }
+            reservedCount += 4;
+        } 
+		else 
+		{
+            int row, col;
+            do 
+			{
+                row = rand() % ROW;
+                col = rand() % COLUMN;
+            } while (seatlist[row][col] != '-');
+            
+            seatlist[row][col] = '@';
+            reservedCount++;
+        }
+    }
+}
+
 
