6872423173a19c7d8e4af4b3dc7bf32a102e2ddc
huiyu0312 <u11002167@o365.ttu.edu.tw>
2024-05-27 00:04:27 +0800

    我完成了1,2,3,4,7題
    這次建立了一個名為people的結構,並宣告3,4題所需要的變數

--- HW4.cpp
+++ HW4.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>//前置處理器的一種,將stdio檔案包含進去
 #include<stdlib.h>//前置處理器的一種,將stdlib檔案包含進去
 #include<string.h>//前置處理器的一種,將string檔案包含進去
+#include<conio.h>//前置處理器的一種,將conio檔案包含進去
 
 void style(void);//個人畫面
  
@@ -9,13 +10,21 @@ void displaying();//顯示學生成績
 void searching();//搜尋學生成績 
 void rankinf();//依學生平均成績的高低順序排列學生資料 
 
+struct people
+{
+	char name[10];
+    int student_number,math,english,physics;
+    float average;
+    char student[10];
+};
+
 
 int main(void)
 {
 	char menu;//宣告選單變數 
 	char o; 
-
-	int x,y=0;//宣告密碼和輸入次數的變數 
+	struct people student[10];
+	int x,y=0,n,i;//宣告密碼和輸入次數的變數 
 	
 	style();
     while(y<3)//最多輸入3次 
@@ -45,6 +54,7 @@ int main(void)
 	 	{
 		   	fflush(stdin);//使input buffer淨空  
 			system("cls");//清除螢幕
+			
 			printf("------[grade system]----------------\n");
 			printf("|   a. enter student grades        |\n");
 			printf("|   b. display student grades      |\n");   
@@ -53,19 +63,82 @@ int main(void)
 			printf("|   e. exit system                 |\n");
 			printf("------------------------------------\n");//顯示主選單
 			
-			
-			
 			printf("INPUT a~e\n");//提醒使用者輸入a~e 
 	        scanf(" %c",&menu);//讀入上方輸入的值 
 	        
 	        switch(menu)
 		    {
 		        case 'a': 
-		            system("pause");
+		            fflush(stdin);
+		            system("cls");
+					
+					printf("input student number(5~10):");
+	                scanf("%d",&n);
+	                if(n<5||n>10)
+	                {
+		             printf("input error,please input again\n");
+		             system("pause");
+	                }
+	                else
+	                {
+		             for(i=1;i<n;i++)
+		                {
+			             printf("input No%d student name:",i);
+			             scanf("%s",student[i].name);
+			
+			             printf("input student number:",i);
+			             scanf("%d",student[i].student_number);
+					 
+					     do
+			             {
+				             printf("input error,please input again");
+			             }
+			             while(student[i].student_number>=999999||student[i].student_number<=100000);
+			
+			             printf("input math grade:");
+			             scanf("%d",student[i].math);
+			
+			             do
+			             {
+				             printf("input error,please input again");
+			             }
+			             while(student[i].math<0||student[i].math>=101);
+			  
+			             printf("input english grade:");
+			             scanf("%d",student[i].english);
+			
+			             do
+			             {
+				             printf("input error,please input again");
+			             }
+			             while(student[i].english<0||student[i].english>=101);
+			
+			             printf("input physics grade:");
+			             scanf("%d",student[i].physics);
+			
+			             do
+			             {
+				             printf("input error,please input again");
+			             }
+			             while(student[i].physics<0||student[i].physics>=101);
+		                }
+	                }
+					
+					student[i].average=(float)(student[i].math+student[i].english+student[i].physics)/3;
+							            
+					system("pause");
 		            break;
 		            
 		        case 'b':
-		            system("cls");
+		            fflush(stdin);
+					system("cls");
+		            
+		            printf("NAME      STUDENT NUMBER        MATH      PHYSICS      ENGLISH      AVERAGE\n");
+		            for(i=0;i<n;i++)
+			        {
+				     printf("%8s%8d%10d%10d%10d%10.2f\n",student[i].name,student[i].student_number,student[i].math,student[i].physics,student[i].english,student[i].average);
+			        }
+			        
 		            system("pause");
 		            break;
 		            
@@ -86,7 +159,7 @@ int main(void)
 						}
 						else if(o=='n')
 						{
-							printf("thank for using\n");
+							printf("thanks for using\n");
 							
 							system("pause");
 							
@@ -148,4 +221,7 @@ void style(void)
     system("cls");//清除螢幕
     
 }
-
+void entering()
+{
+	
+}
